1fdcb3a542c5001720ce8ecbbdf51a419090814c
josiahyeow <jsiahyeow@gmail.com>
2018-12-27 03:11:19 +1100

    Initial commit for portfolio site

---
+++ main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        linkedIn: 'https://www.linkedin.com/in/josiah-yeow/',
+        github: 'https://github.com/josiahyeow',
+        email: 'jsiahyeow@gmail.com',
+        apps: [
+            {name: 'PocketLint', image: './images/pocketlint-icon.png', url:'#', type: 'iOS', desc: 'Image Scanner App'},
+            {name: 'Flush', image: './images/flush-icon.png', url:'#', type: 'Android', desc: 'Task manager App'}
+        ],
+        designs: [
+            {name: 'Property Search', image: './images/property-screens.png', desc: ''},
+            {name: 'Real Estate', image: './images/realestate-screens.png', desc: ''},
+            {name: 'Flush Design', image: './images/flush-screens.png', desc: ''}
+        ]
+    },
+    computed: {
+        age: function() {
+            var year = new Date().getFullYear();
+            var birthyear = 1996;
+            return year - birthyear;
+        }
+    }
+})
