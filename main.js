18a25f87d36821ba8d53d681992c5a4e333cf9fb
josiahyeow <jsiahyeow@gmail.com>
2019-01-11 17:01:14 +1100

    fix date calculation to account for months

--- main.js
+++ main.js
@@ -16,9 +16,14 @@ var app = new Vue({
     },
     computed: {
         age: function() {
-            var year = new Date().getFullYear();
-            var birthyear = 1996;
-            return year - birthyear;
+            var today = new Date();
+            var birthDate = new Date("1996/09/27");
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+            }
+            return age;
         }
     }
 })
